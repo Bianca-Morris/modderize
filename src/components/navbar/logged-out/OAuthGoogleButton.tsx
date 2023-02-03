016bb84cf3011f90ec880a936b41b36e87b1489d
Bianca Morris <biancaallynm@gmail.com>
2023-02-03 03:36:56 -0500

    Add ability to register w/Google

--- src/components/navbar/logged-out/OAuthGoogleButton.tsx
+++ src/components/navbar/logged-out/OAuthGoogleButton.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+
+import { auth } from "../../../firebase/clientApp";
 import Button from "../../basic/Button";
 
 const OAuthGoogleButton: React.FC = () => {
+	const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
 	return (
-		<Button variant="grayOutline" onClick={() => null}>
-			<img src="/images/Google_Logo.svg"></img>Continue with Google
-		</Button>
+		<>
+			<Button
+				variant="grayOutline"
+				onClick={() => signInWithGoogle()}
+				{...{ loading }}
+			>
+				<img src="/images/Google_Logo.svg"></img>Continue with Google
+			</Button>
+			{error && (
+				<div className="text-sm text-center text-red-600 mt-2">
+					{error.message}
+				</div>
+			)}
+		</>
 	);
 };
 export default OAuthGoogleButton;
