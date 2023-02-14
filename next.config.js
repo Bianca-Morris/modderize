735be3a12191229d931191ba4e2d9c23f0e8dff7
Bianca Morris <biancaallynm@gmail.com>
2023-02-14 08:56:46 -0500

    Add FramerX Support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
+	experimental: {
+		urlImports: [
+			"https://framer.com/m/",
+			"https://framerusercontent.com/",
+			"https://fonts.gstatic.com/",
+			"https://ga.jspm.io/",
+			"https://jspm.dev/"
+		]
+	}
 };
