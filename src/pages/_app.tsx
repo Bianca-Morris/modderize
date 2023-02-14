c2388b5745ca8a0e303a515f06c77a529a1efe21
Bianca Morris <biancaallynm@gmail.com>
2023-02-14 08:57:12 -0500

    Test framer import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
 import { RecoilRoot } from "recoil";
+import CardsSearchResult_Modder from "https://framer.com/m/Cards-SearchResult-Modder-Ziap.js@EuFW80XbgjhlwQZxAi0n";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
 			<Layout>
 				<Component {...pageProps} />
+				<CardsSearchResult_Modder />
 			</Layout>
 		</RecoilRoot>
 	);
