32052d96f65f60c2dac1a031d602a7a36d88fab7
Bianca Morris <biancaallynm@gmail.com>
2023-02-14 08:57:34 -0500

    Update name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center py-2">
 			<Head>
-				<title>Create Next App</title>
+				<title>Modderize</title>
 				<link rel="icon" href="/favicon.ico" />
 				<link
 					rel="stylesheet"
