f9b3b4d64b12cb2cb9e358363c889b1946e65e98
Bianca Morris <biancaallynm@gmail.com>
2023-02-20 18:39:28 -0500

    Test some more Framer stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
 import { RecoilRoot } from "recoil";
-import CardsSearchResult_Modder from "https://framer.com/m/Cards-SearchResult-Modder-Ziap.js@EuFW80XbgjhlwQZxAi0n";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
 			<Layout>
 				<Component {...pageProps} />
-				<CardsSearchResult_Modder />
 			</Layout>
 		</RecoilRoot>
 	);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/clientApp";
+
+import CardsSearchResult_Modder from "https://framer.com/m/Cards-SearchResult-Modder-Ziap.js@EuFW80XbgjhlwQZxAi0n";
+import FeaturesJumbotronWithButtons from "https://framer.com/m/Features-JumbotronWithButtons-U3un.js@Nt1jyKYpPeYLViU5bfrE";
 
 const Home: NextPage = () => {
+	const [user, loadingUser] = useAuthState(auth);
+
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center py-2">
 			<Head>
@@ -12,6 +19,18 @@ const Home: NextPage = () => {
 					href="https://rsms.me/inter/inter.css"
 				></link>
 			</Head>
+			{/* <FeaturesJumbotronWithButtons
+				// Using default values:
+				jumbotronText="Lorem ipsum dolor sit amet. Ex maiores magnam in quis dignissimos quo quisquam accusamus. Ut fuga fugit qui provident velit et eaque deleniti in asperiores quibusdam. Et expedita debitis sed sint nobis sed dolores illo vel nemo deserunt."
+				jumbotronTitle="Your Wildest Dreams Come True"
+				onTapFulfillRequest={undefined}
+				onTapModRequest={undefined}
+			/> */}
+
+			{user && (
+				<h1 className="mb-8 text-xl">{"Hello, " + user.displayName}</h1>
+			)}
+			<CardsSearchResult_Modder />
 		</div>
 	);
 };
