af9defd6fbabe69f545da864a2428bc8fc4a9e75
Bianca Morris <biancaallynm@gmail.com>
2023-03-18 15:49:11 -0400

    Improve Game Not Found page

--- src/pages/games/[gameID]/index.tsx
+++ src/pages/games/[gameID]/index.tsx
@@ -22,22 +22,32 @@ const GamePage: React.FC<GamePageProps> = ({ gameData }) => {
 	console.log("gameData", gameData);
 	if (!gameData) {
 		return (
-			<div className="flex min-h-screen flex-col item-center justify-center py-2">
+			<div className="flex min-h-screen flex-col item-center justify-start pb-2">
 				<SimpleHeader>
 					<div className="flex text-center text-3xl">
 						<h1>Error: Game Not Found</h1>
 					</div>
 				</SimpleHeader>
-				<div>
-					<h1>Sorry!</h1>
-					<h2>This game doesn't seem to be on Modderize.</h2>
-					<p>
-						Check the spelling of the ID or{" "}
-						<a href="/browse">Browse by Game</a>.
-					</p>
-					<Button type="button" variant="gray">
-						Request a Game
-					</Button>
+				<div className="w-full flex mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 gap-16">
+					<div className="py-6">
+						<h1 className="text-xl font-bold font-medium text-gray-900 mb-3">
+							Sorry!
+						</h1>
+						<h2>This game doesn't seem to be on Modderize.</h2>
+						<p>
+							Check the spelling of the ID or&nbsp;
+							<a
+								className="underline text-gray-500"
+								href="/browse"
+							>
+								Browse by Game
+							</a>
+							.
+						</p>
+						<Button type="button" variant="gray" cls="mt-3">
+							Request a Game
+						</Button>
+					</div>
 				</div>
 			</div>
 		);
