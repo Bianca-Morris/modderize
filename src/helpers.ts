26537d488c93809bcf3f50f6337e833e29566ec9
Bianca Morris <biancaallynm@gmail.com>
2023-04-21 02:21:35 -0400

    Update helpers + change extension

---
+++ src/helpers.ts
@@ -0,0 +1,139 @@
+import {
+	collection,
+	limit,
+	orderBy,
+	Query,
+	query,
+	QueryDocumentSnapshot,
+	QuerySnapshot,
+	startAfter,
+	where
+} from "firebase/firestore";
+import { defaultSearchState } from "./atoms/searchAtom";
+import { firestore } from "./firebase/clientApp";
+
+export function validateEmail(email) {
+	// Regex from https://www.tutorialspoint.com/How-to-validate-email-address-in-JavaScript
+	const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if (email.match(mailformat)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// Takes in a bunch of class names as strings and creates a single string for tailwind
+export function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
+
+/**
+ * Search Utilities
+ */
+
+// export const acceptableSortFields = {
+// 	modRequests: ["title", "creationDate", "lastModified", "voteStatus"],
+// 	games: ["displayName", "numberOfPlayers"],
+// 	users: ["displayName"]
+// };
+
+// export const acceptableSortValues = ["asc", "desc"];
+
+// export function isValidQuery(queryFromContext) {
+// 	const {
+// 		type = defaultSearchState.docType,
+// 		q = "",
+// 		sortField = null,
+// 		sortValue = null,
+// 		p = 1
+// 	} = queryFromContext || {};
+
+// 	const invalidQueryType =
+// 		type !== "modRequests" && type !== "games" && type !== "users";
+// 	if (
+// 		// If type is invalid
+// 		invalidQueryType ||
+// 		// If sort fields are incompatible with type
+// 		(sortField && !acceptableSortFields[type].includes(sortField)) ||
+// 		// If sort value is incompatible with type
+// 		(sortValue && !acceptableSortValues.includes(sortValue))
+// 		// TODO: Validate filter fields
+// 	) {
+// 		return false;
+// 	}
+// 	// Query should be valid
+// 	return true;
+// }
+
+const DOCUMENTS_PER_PAGE = 15;
+
+
+export function constructModRequestQuery(prevDocSnaps?: QuerySnapshot) {
+	// Query the games collection
+	const coll = collection(firestore, "modRequests");
+
+	// Some basic values for all sort (TODO: Maybe make these dynamic?)
+	const sortField = "title";
+	const sortValue = "asc";
+
+	// Construct a new query
+	let returnQuery: Query = query(
+		coll,
+		orderBy(sortField, sortValue),
+		limit(DOCUMENTS_PER_PAGE)
+	);
+
+	// If there is a previous query, update the current one to start after that cursor
+	if (prevDocSnaps) {
+		const lastVisible = getLastVisibleDocument(prevDocSnaps);
+		returnQuery = query(returnQuery, startAfter(lastVisible));
+	}
+
+	return returnQuery;
+}
+
+export function constructGameQuery(
+	keyword?: string | string[],
+	prevDocSnaps?: QuerySnapshot
+) {
+	// Query the games collection
+	const coll = collection(firestore, "games");
+
+	// Some basic values for all sort (TODO: Maybe make these dynamic?)
+	const sortField = "displayName";
+	const sortValue = "asc";
+	
+	// Construct a new query
+	let returnQuery: Query;
+
+	// Take into account any keywords
+	if (keyword && typeof keyword === "string") {
+		returnQuery = query(
+			coll,
+			where("displayName", "==", keyword),
+			orderBy(sortField, sortValue),
+			limit(DOCUMENTS_PER_PAGE)
+		);
+	} else {
+		console.log("found no keyword", keyword);
+		returnQuery = query(
+			coll,
+			orderBy(sortField, sortValue),
+			limit(DOCUMENTS_PER_PAGE)
+		);
+	}
+
+	// If there is a previous query, update the current one to start after that cursor
+	if (prevDocSnaps) {
+		const lastVisible = getLastVisibleDocument(prevDocSnaps);
+		returnQuery = query(returnQuery, startAfter(lastVisible));
+	}
+
+	return returnQuery;
+}
+
+
+export function getLastVisibleDocument(documentSnapshots: QuerySnapshot): QueryDocumentSnapshot {
+	const lastIndex = documentSnapshots.docs.length-1;
+	return documentSnapshots.docs[lastIndex];
+}
