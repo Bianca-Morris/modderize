362ea8fd6c743b435fc3fc270dc9a529c50297af
Bianca Morris <biancaallynm@gmail.com>
2023-04-28 23:52:07 -0400

    Fix some comments

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
+// Import the functions needed from various SDKs
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
