15756aea1d53331873ca69b3a0184e26986136c0
Bianca Morris <biancaallynm@gmail.com>
2023-05-06 18:32:17 -0400

    Tweak to SimpleHeader

--- src/components/general/SimpleHeader.tsx
+++ src/components/general/SimpleHeader.tsx
@@ -2,10 +2,7 @@ import React, { PropsWithChildren } from "react";
 
 const SimpleHeader: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<div
-			className="w-full text-white p-10"
-			style={{ backgroundColor: "darkslateblue" }}
-		>
+		<div className="w-full text-white p-10 bg-[#483D8B]">
 			<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 				{children}
 			</div>
