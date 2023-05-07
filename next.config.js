0d458d34910f5336cc6dc150cdd11e577b30b85e
Bianca Morris <biancaallynm@gmail.com>
2023-05-07 14:30:28 -0400

    Remove Framer

--- next.config.js
+++ next.config.js
@@ -16,14 +16,5 @@ module.exports = {
 			}
 		]
 	},
-	reactStrictMode: true,
-	experimental: {
-		urlImports: [
-			"https://framer.com/m/",
-			"https://framerusercontent.com/",
-			"https://fonts.gstatic.com/",
-			"https://ga.jspm.io/",
-			"https://jspm.dev/"
-		]
-	}
+	reactStrictMode: true
 };
