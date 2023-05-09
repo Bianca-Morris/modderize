8b5291ecbc0693ef14f904547e15a482f7f952ee
Bianca Morris <biancaallynm@gmail.com>
2023-05-09 00:10:41 -0400

    Update Game Not Found error

--- src/components/pages/GameNotFound.tsx
+++ src/components/pages/GameNotFound.tsx
@@ -1,37 +1,23 @@
 import React from "react";
+import ErrorPage from "../../errors/ErrorPage";
 import A from "../basic/A";
-import Button from "../basic/Button";
-import H1 from "../basic/typography/H1";
-import H2 from "../basic/typography/H2";
-import H3 from "../basic/typography/H3";
-import SimpleHeader from "../general/SimpleHeader";
-import ContentBody from "../layout/ContentBody";
 
 const GameNotFoundPage: React.FC = () => {
 	return (
-		<ContentBody>
-			<SimpleHeader>
-				<div className="flex text-center">
-					<H1>Error: Game Not Found</H1>
-				</div>
-			</SimpleHeader>
-			<>
-				<div className="py-6">
-					<H2 cls="font-bold text-gray-900 mb-3">Sorry!</H2>
-					<H3>This game doesn&apos;t seem to be on Modderize.</H3>
-					<p>
-						Check the spelling of the ID or&nbsp;
-						<A variant="gray" href="/games">
-							Browse by Game
-						</A>
-						.
-					</p>
-					<Button type="button" variant="gray" cls="mt-3">
-						Request a Game
-					</Button>
-				</div>
-			</>
-		</ContentBody>
+		<ErrorPage
+			header1="Error: Game Not Found"
+			header2="Sorry!"
+			header3="This game doesn't seem to be on Modderize."
+			paragraph={
+				<>
+					Check the spelling of the ID or&nbsp;
+					<A variant="gray" href="/games">
+						Browse by Game
+					</A>
+					.
+				</>
+			}
+		/>
 	);
 };
 
