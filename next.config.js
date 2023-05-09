df194a134171f0ebebaa8d0d7ee36ed1fb02bec4
Bianca Morris <biancaallynm@gmail.com>
2023-05-08 21:56:47 -0400

    Unignore linter on build

--- next.config.js
+++ next.config.js
@@ -16,8 +16,5 @@ module.exports = {
 			}
 		]
 	},
-	reactStrictMode: true,
-	// Warning: This allows production builds to successfully complete even if
-	// your project has ESLint errors.
-	ignoreDuringBuilds: true
+	reactStrictMode: true
 };
